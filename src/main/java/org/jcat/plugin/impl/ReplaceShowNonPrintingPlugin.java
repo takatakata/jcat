a00998b385ef84bc4b4eb411265542f660e076d6
s9910 <s9910@192.168.11.109>
2025-04-28 11:53:07 +0900

    処理をコードポイント単位からバイト単位に変更することでnon-printingの再現性を向上。 sample1.txtの誤記を訂正。

--- src/main/java/org/jcat/plugin/impl/ReplaceShowNonPrintingPlugin.java
+++ src/main/java/org/jcat/plugin/impl/ReplaceShowNonPrintingPlugin.java
@@ -1,6 +1,8 @@
 package org.jcat.plugin.impl;
 
-import java.util.stream.IntStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import org.jcat.core.context.StreamContext;
 import org.jcat.plugin.AbstractReplacePlugin;
@@ -13,14 +15,56 @@ public class ReplaceShowNonPrintingPlugin extends AbstractReplacePlugin {
 
     @Override
     public String replaceLine(StreamContext context, String src) {
-    	return convertAsASCII(src);
+    	return convertAsBytes(src);
     }
     
 	@Override
 	public boolean isEnabled() {
 		return option.isShowNonPrinting();
 	}
-	
+
+    private String convertAsBytes(String src) {
+        StringBuffer buf = new StringBuffer();
+        byte[] bytes;
+        try {
+            bytes = src.getBytes("UTF-8");
+            try (InputStream input = new ByteArrayInputStream(bytes)) {
+                int ch;
+                while ((ch = input.read()) != -1) {
+                    if (ch >= 32) {
+                        if (ch < 127) {
+                            buf.appendCodePoint(ch);
+                        } else if (ch == 127) {
+                            buf.append("^?");
+                        } else {
+                            if (ch >= 128 + 32) {
+                                if (ch < 128 + 127) {
+                                    buf.append("M-");
+                                    buf.appendCodePoint(ch - 128);
+                                } else {
+                                    buf.append("^?");
+                                }
+                            } else {
+                                buf.append("M-");
+                                buf.append("^").appendCodePoint(ch - 128 + 64);
+                            }
+                        }
+                    } else if (ch == '\t' && !option.isShowTabs()) {
+                        buf.append("\t");
+                    } else if (ch == '\n') {
+                        buf.append("\n");
+                    } else {
+                        buf.append("^").appendCodePoint(ch + 64);
+                    }
+                }
+            }
+            return buf.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return src;
+        }
+    }
+
     /**
      * https://github.com/coreutils/coreutils/blob/master/src/cat.c
      *   // If quoting, i.e., at least one of -v, -e, or -t specified,
@@ -76,6 +120,7 @@ public class ReplaceShowNonPrintingPlugin extends AbstractReplacePlugin {
      *         }
      *     }
      */
+
 //	private String convertAsCodepoint(String src) {
 //	    StringBuffer buf = new StringBuffer();
 //        try (IntStream input = src.codePoints()) {
@@ -111,40 +156,4 @@ public class ReplaceShowNonPrintingPlugin extends AbstractReplacePlugin {
 //        }
 //        return buf.toString();
 //	}
-    private String convertAsASCII(String src) {
-        StringBuffer buf = new StringBuffer();
-        try (IntStream input = src.chars()) {
-            input.forEach(ch -> {
-                if (ch >= 32) {
-                    if (ch < 127) {
-                        buf.appendCodePoint(ch);
-                    } else if (ch == 127) {
-                        buf.append("^?");
-                    } else {
-                        if (ch >= 128 + 32) {
-                            if (ch < 128 + 127) {
-                                buf.append("M-");
-                                buf.appendCodePoint(ch - 128);
-                            } else {
-                                // treat ch as a Unicode character, not just an ASCII character 
-                                // original code: `buf.append("^?")`
-                                //buf.appendCodePoint(ch);
-                                buf.appendCodePoint(ch);
-                            }
-                        } else {
-                            buf.append("M-");
-                            buf.append("^").appendCodePoint(ch - 128 + 64);
-                        }
-                    }
-                } else if (ch == '\t' && !option.isShowTabs()) {
-                    buf.append("\t");
-                } else if (ch == '\n') {
-                    buf.append("\n");
-                } else {
-                    buf.append("^").appendCodePoint(ch + 64);
-                }
-            });
-        }
-        return buf.toString();
-    }
 }
